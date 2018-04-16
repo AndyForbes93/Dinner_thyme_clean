9293623e4e340970d1167c2f1e58ae1b674e7acd
Andy Forbes <33873838+AndyForbes93@users.noreply.github.com>
2018-04-16 17:05:53 -0400

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -182,3 +182,5 @@ $(document).ready(function () {
         // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         $('.modal-trigger').modal();
     });
+  });
+})
