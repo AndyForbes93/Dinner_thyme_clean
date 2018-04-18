0b357b82968f7d62e593d160576f1a28170afa48
Andy Forbes <33873838+AndyForbes93@users.noreply.github.com>
2018-04-18 09:37:06 -0400

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -300,6 +300,12 @@ $(document).ready(function () {
         });
 
     })
+    
+        $('.carousel.carousel-slider').carousel({
+        fullWidth: true, 
+        indicators: true,
+        duration: 200
+    });
 
     $(document).ready(function () {
         // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
